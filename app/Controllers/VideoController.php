c6eb94643e6b20705628dd36ec9c81d5cc6d235c
pmb2020 <pmb2020@163.com>
2020-07-01 10:43:21 +0800

    '获取影视分类信息接口'

--- app/Controllers/VideoController.php
+++ app/Controllers/VideoController.php
@@ -23,22 +23,104 @@ class VideoController{
     public function dianyingType(){
         echo $this->reposeJson(200,'电影类型获取成功！',$this->dianyingTypeAll('type'));
     }
-    public function dianyingTypeAll($type){
-        $url='https://www.360kan.com/dianying/list';
-        $html = preg_replace("/\r|\n|\t/","",$this->curlAlone($url));
-        $rule='/<a class="js-tongjip" href=\"(.*)" target="_self">(.*)<\/a>/iU';
-        preg_match_all($rule,$html,$typeData);
-        preg_match('/[\x7f-\xff]+/',$typeData[2][0],$name);
-        for ($i=0;$i < count($typeData[1]);$i++){
-            preg_match('/[\x7f-\xff]+/',$typeData[2][$i],$name);
-            if (!$name){
-                $name[0]=$typeData[2][$i];
-            }
-            $arr[]=[
-                'name' => $name[0],
-                'url' => $typeData[1][$i]
-            ];
+//    电视年份
+    public function dianshiYear(){
+        echo $this->reposeJson(200,'电视年份获取成功！',$this->dianshiTypeAll('year'));
+    }
+//    电视演员
+    public function dianshiStar(){
+        echo $this->reposeJson(200,'电视演员获取成功！',$this->dianshiTypeAll('star'));
+    }
+//    电视地域
+    public function dianshiCountry(){
+        echo $this->reposeJson(200,'电视地域获取成功！',$this->dianshiTypeAll('country'));
+    }
+//    电视类型
+    public function dianshiType(){
+        echo $this->reposeJson(200,'电视类型获取成功！',$this->dianshiTypeAll('type'));
+    }
+//    综艺类型
+    public function zongyiType(){
+        echo $this->reposeJson(200,'综艺类型获取成功！',$this->zongyiTypeAll('type'));
+    }
+//    综艺地域
+    public function zongyiCountry(){
+        echo $this->reposeJson(200,'综艺地域获取成功！',$this->zongyiTypeAll('country'));
+    }
+//    综艺演员
+    public function zongyiStar(){
+        echo $this->reposeJson(200,'综艺演员获取成功！',$this->zongyiTypeAll('star'));
+    }
+    public function dianshiTypeAll($type){
+        $data=$this->pregData('https://www.360kan.com/dongman/list');
+        $part1=array_chunk($data, 45);
+        $part2=array_chunk($part1[0], 36);
+        $part3=array_chunk($part2[0], 21);
+//        $this->dd($part3);
+        $starData=$part1[1];
+        $countryData=$part2[1];
+        $yearData=$part3[1];
+        $typeData=$part3[0];
+        switch ($type){
+            case 'year':
+                return $data;
+            case 'star':
+                return $starData;
+            case 'country':
+                return $countryData;
+            default:
+                return $typeData;
         }
+    }
+    public function zongyiTypeAll($type){
+        $data=$this->pregData('https://www.360kan.com/zongyi/list');
+        $part1=array_chunk($data, 55);
+        $part2=array_chunk($part1[0], 24);
+//        $part3=array_chunk($part2[0], 21);
+//        $this->dd($data);
+        $starData=array_merge($part2[1],$part2[2]);
+        $countryData=$part1[1];
+        $typeData=$part2[0];
+        switch ($type){
+            case 'star':
+                return $starData;
+            case 'country':
+                return $countryData;
+            default:
+                return $typeData;
+        }
+    }
+    public function dongmanTypeAll(){
+        $data=$this->pregData('https://www.360kan.com/dongman/list');
+        $part1=array_chunk($data, 53);
+        $part2=array_chunk($part1[0], 35);
+        $countryData=$part1[1];
+        $yearData=$part2[1];
+        $typeData=$part2[0];
+        $arr=[
+            'type'=>$typeData,
+            'year' => $yearData,
+            'country'=>$countryData
+        ];
+        echo $this->reposeJson(200,'动漫分类信息获取成功！',$arr);
+    }
+    public function dianyingTypeAll($type){
+//        $url='https://www.360kan.com/dianying/list';
+//        $html = preg_replace("/\r|\n|\t/","",$this->curlAlone($url));
+//        $rule='/<a class="js-tongjip" href=\"(.*)" target="_self">(.*)<\/a>/iU';
+//        preg_match_all($rule,$html,$typeData);
+//        preg_match('/[\x7f-\xff]+/',$typeData[2][0],$name);
+//        for ($i=0;$i < count($typeData[1]);$i++){
+//            preg_match('/[\x7f-\xff]+/',$typeData[2][$i],$name);
+//            if (!$name){
+//                $name[0]=$typeData[2][$i];
+//            }
+//            $arr[]=[
+//                'name' => $name[0],
+//                'url' => $typeData[1][$i]
+//            ];
+//        }
+        $arr=$this->pregData('https://www.360kan.com/dianying/list');
         $part1=array_chunk($arr, 47);
         $part2=array_chunk($part1[0], 35);
         $part3=array_chunk($part2[0], 20);
@@ -60,15 +142,25 @@ class VideoController{
 //        $this->dd($typeData);
 //        echo $this->reposeJson('200','success',$arr);
     }
-    function reposeJson($code,$msg,$data){
-        $respose=[
-            'code' => $code,
-            'msg' => $msg,
-            'data' => $data
-        ];
-        return json_encode($respose,JSON_UNESCAPED_UNICODE);
+
+    function pregData($url){
+//        $url='https://www.360kan.com/dianying/list';
+        $html = preg_replace("/\r|\n|\t/","",$this->curlAlone($url));
+        $rule='/<a class="js-tongjip" href=\"(.*)" target="_self">(.*)<\/a>/iU';
+        preg_match_all($rule,$html,$typeData);
+        preg_match('/[\x7f-\xff]+/',$typeData[2][0],$name);
+        for ($i=0;$i < count($typeData[1]);$i++){
+            preg_match('/[\x7f-\xff]+/',$typeData[2][$i],$name);
+            if (!$name){
+                $name[0]=$typeData[2][$i];
+            }
+            $arr[]=[
+                'name' => $name[0],
+                'url' => $typeData[1][$i]
+            ];
+        }
+        return $arr;
     }
-    function pregData(){}
     function curlAlone($url){
         $ch=curl_init($url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -80,7 +172,14 @@ class VideoController{
         curl_close($ch);
         return $content;
     }
-
+    function reposeJson($code,$msg,$data){
+        $respose=[
+            'code' => $code,
+            'msg' => $msg,
+            'data' => $data
+        ];
+        return json_encode($respose,JSON_UNESCAPED_UNICODE);
+    }
     function dd($data)
     {
         // 定义样式
--- route/api.php
+++ route/api.php
@@ -12,4 +12,15 @@ Route::get('/v1/video/dianying/year','VideoController@dianyingYear');
 Route::get('/v1/video/dianying/star','VideoController@dianyingStar');
 Route::get('/v1/video/dianying/country','VideoController@dianyingCountry');
 Route::get('/v1/video/dianying/type','VideoController@dianyingType');
+
+Route::get('/v1/video/dianshi/year','VideoController@dianshiYear');
+Route::get('/v1/video/dianshi/star','VideoController@dianshiStar');
+Route::get('/v1/video/dianshi/country','VideoController@dianshiCountry');
+Route::get('/v1/video/dianshi/type','VideoController@dianshiType');
+
+Route::get('/v1/video/zongyi/star','VideoController@zongyiStar');
+Route::get('/v1/video/zongyi/country','VideoController@zongyiCountry');
+Route::get('/v1/video/zongyi/type','VideoController@zongyiType');
+
+Route::get('/v1/video/dongman/type/all','VideoController@dongmanTypeAll');
 //Route::get('/v2/get','VideoController@test');
